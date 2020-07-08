34cf50541fd9eb47142ae60e01be31862642055e
Lars Ivar Hatledal <laht@hials.no>
2020-07-08 22:47:06 +0200

    refactor deps

--- plugin/src/main/kotlin/no/ntnu/ihb/sspgen/SSPGenPlugin.kt
+++ plugin/src/main/kotlin/no/ntnu/ihb/sspgen/SSPGenPlugin.kt
@@ -7,9 +7,9 @@ import java.net.URL
 
 open class SSPGenPluginExtension {
 
-    val outputDir: String? = null
-    val urls = mutableListOf<String>()
-    val files = mutableListOf<String>()
+    var outputDir: String? = null
+    var urls = mutableListOf<String>()
+    var files = mutableListOf<String>()
 
 }
 
@@ -18,13 +18,19 @@ class SSPGenPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.extensions.create("sspgen", SSPGenPluginExtension::class.java).also { ext ->
 
-            val outputDir: File? = ext.outputDir?.let { File(it) }
+            val task = project.task("invoke")
+            task.group = "sspgen"
+            task.doLast {
+                val outputDir: File? = ext.outputDir?.let { File(it) }
 
-            ext.urls.map { URL(it) }.forEach { url ->
-                evaluateScript(url.openStream(), outputDir)
-            }
-            ext.files.map { File(it) }.forEach { file ->
-                evaluateScript(file.inputStream(), outputDir)
+                ext.urls.map { URL(it) }.forEach { url ->
+                    println("Downloading sspgen-definitions '$it'")
+                    evaluateScript(url.openStream(), outputDir)
+                }
+                ext.files.map { File(it) }.forEach { file ->
+                    println("Downloading sspgen-definitions '$it'")
+                    evaluateScript(file.inputStream(), outputDir)
+                }
             }
 
         }
