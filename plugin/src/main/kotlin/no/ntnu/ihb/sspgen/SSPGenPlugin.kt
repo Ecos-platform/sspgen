3aba9496367854637fa1dcccb1a9234e3dfcf86b
Lars Ivar Hatledal <laht@hials.no>
2020-07-08 21:54:57 +0200

    plugin wip

--- plugin/src/main/kotlin/no/ntnu/ihb/sspgen/SSPGenPlugin.kt
+++ plugin/src/main/kotlin/no/ntnu/ihb/sspgen/SSPGenPlugin.kt
@@ -2,17 +2,32 @@ package no.ntnu.ihb.sspgen
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.io.File
+import java.net.URL
 
 open class SSPGenPluginExtension {
 
+    val outputDir: String? = null
     val urls = mutableListOf<String>()
+    val files = mutableListOf<String>()
 
 }
 
 class SSPGenPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        val extension = project.extensions.create("sspgen", SSPGenPluginExtension::class.java)
+        project.extensions.create("sspgen", SSPGenPluginExtension::class.java).also { ext ->
+
+            val outputDir: File? = ext.outputDir?.let { File(it) }
+
+            ext.urls.map { URL(it) }.forEach { url ->
+                evaluateScript(url.openStream(), outputDir)
+            }
+            ext.files.map { File(it) }.forEach { file ->
+                evaluateScript(file.inputStream(), outputDir)
+            }
+
+        }
     }
 
 }
